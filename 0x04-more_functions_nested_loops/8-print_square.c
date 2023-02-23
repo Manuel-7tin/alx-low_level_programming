a0ddf42fe725d64a298de3a4fe358591b9cc1a5b
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-02-23 15:36:08 -0800

    Task 8: This program prints a square of specified size using #

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,11 +10,13 @@
 
 void print_square(int size)
 {
+	int loop1, loop2;
+
 	if (size <= 0)
 		_putchar('\n');
-	for (; size > 0; size--)
+	for (loop1 = size; loop1 > 0; loop1--)
 	{
-		for (; size > 0; size--)
+		for (loop2 = size; loop2 > 0; loop2--)
 			_putchar('#');
 		_putchar('\n');
 	}

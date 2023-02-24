4dc5e6c4dffcbeb98c00288cb65489cbf39d165d
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-02-23 19:28:15 -0800

    A fizz buzz gme

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ int main(void)
 	for (number = 1; number < 101; number++)
 	{
 		if (number % 3 == 0 && number % 5 == 0)
-			printf("Fizzbuzz");
+			printf("Fizz buzz");
 		else if (number % 3 == 0)
 			printf("Fizz");
 		else if (number % 5 == 0)

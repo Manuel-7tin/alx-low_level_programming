abda741dfa886b7375afd204deca8d16db1653b4
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-02-28 03:24:11 -0800

    Task1: Swaps the value of variables using pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the value of variables using pointers
+ *
+ * @a: Variable1 to be swaped
+ * @b: Variable12 to be swapped
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	c = a;
+	a = b;
+	b = c;
+}

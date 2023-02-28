a781626827173df02a822021af7c3a44ba7f9356
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-02-28 12:53:25 -0800

    Task3: Prints a strinng from it's memory location

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a strinng from it's memory location
+ *
+ * @str: string location to be printed
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	printf("%c\n", str);
+}

7f0f2a09fe70d47f698ca2821621e045c420d7b2
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-02-28 04:09:16 -0800

    Task2: Returns the length of an inputted string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of an inputted string
+ *
+ * @s: Inputed string
+ *
+ * Return: Length of the string
+ */
+
+int _strlen(char *s)
+{
+	int num;
+
+	num = strlen(*s)
+	return (num)
+}

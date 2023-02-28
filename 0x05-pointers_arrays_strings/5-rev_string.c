ea3f3f14dc78624dac0e1959aa498fa9886d163f
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-02-28 14:56:02 -0800

    Task5: Prints strings backwards

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string frtom it's allocated memory
+ * _putchar - print each character
+ * @str: char to check
+ *
+ * Description: This will reverse a string
+ * Return: 0 is success
+ */
+void rev_string(char *str)
+{
+	int a = 0, b, c;
+	char d;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	c = a - 1;
+	for (b = 0; c >= 0 && b < c; c--, b++)
+	{
+		d = str[b];
+		str[b] = str[c];
+		str[c] = d;
+	}
+}

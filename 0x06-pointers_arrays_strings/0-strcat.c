cc10adb8ebd3bc13dfb94749912407307a823e0e
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-03-02 06:15:03 -0800

    Task0: Concatenates two inputted strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * _strcat - Function concatenates two imputted strings
- *
- * @dest: First string input to be concatenated
- * @src: Second string input to be concatenated
- *
- * Return: Char (Conctenated string)
+ * _strcat - concat 2 string
+ * @dest:char
+ * @src:char
+ * Return:char
  */
-
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
-	int n, len1, len2 = 0;
+	char *s = dest;
 
-	for (n = 0; dest[n] != '\0'; n++)
-		len1++;
-	for (n = 0; src[n] != '\0'; n++)
-		len2++;
-	for (n = 0; n < len2; n++)
+	while (*dest != '\0')
 	{
-		dest[len1 + 1] = src[n];
-		len1++;
+		dest++;
 	}
-	return (&dest);
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
 }

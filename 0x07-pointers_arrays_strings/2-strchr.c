aa5b2a8729661022027916b36adf3774e60afd90
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-03-06 07:34:26 -0800

    Task@: A function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - A function that locates a character in a string
+ *
+ * @s: The string to be searched
+ * @c: The character to be located
+ *
+ * Return: Char location (Succesful) Null (Failed)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (c == s[i])
+		{
+			break;
+			return (s[i]);
+		}
+	}
+	return (NULL);
+}

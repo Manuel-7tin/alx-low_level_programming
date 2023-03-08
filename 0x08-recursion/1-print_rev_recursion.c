afcfc43662693566006f9debbb80b6f3da2e838e
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-03-08 07:29:38 -0800

    Task1: function that prints a string in reverse.

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints inputted string in reverse
+ *
+ * @s: String to be printed
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (s[0])
+	{
+		_print_rev_recursion(++s);
+		_putchar(s[0]);
+	}
+}

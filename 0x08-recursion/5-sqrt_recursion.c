720a109e5a597c9e12c2847fbbacdcf9d8ae7e6f
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-03-08 23:01:47 -0800

    Task5: A function that returns the natural square root of a number using recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * check_for_square - checks for the square root
+ *
+ * @x: aids with the calculation
+ * @y: Int whose root we shalll determine
+ *
+ * Return: int (the root)
+ */
+
+int check_for_square(int x, int y)
+{
+	int root = (x + y)/2;
+
+	if (root * root == y)
+		return (root);
+	else
+		return (check_for_square(x - 1, y));
+}
+
+
+/**
+ * _sqrt_recursion - Determines the square root of an inputted integer
+ *
+ * @n: The integer whose square root will be returned
+ *
+ * Return: int (rooot of n)
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (check_for_square(0, n));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * check_for_prime - Checks for prime numbers
+ *
+ * @x: aids with the calculation
+ * @y: :w
+/**
+ * is_prime_number - Determines a prime number

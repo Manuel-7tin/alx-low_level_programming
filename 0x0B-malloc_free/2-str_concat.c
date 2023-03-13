1b762896c630b24b2e342869f01f00d18854cdcc
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-03-13 07:39:12 -0700

    Task2: A function that concactenates two inputted strings - malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 	char *ptr_concat_str;
 	unsigned int i;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return ("");
 	ptr_concat_str = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 	if (ptr_concat_str == NULL)

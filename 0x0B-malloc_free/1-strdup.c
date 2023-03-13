887bf253b60165d5639b164af75bffc3a295a8df
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-03-13 06:12:30 -0700

    Task1: A function that duplicates an inputted string using malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - Creates a duplicate of an inputted string
+ *
+ * @str: The inputted string
+ *
+ * Return: A pointer to the duplicate (if successful) | Null (if not)
+ */
+
+char *_strdup(char *str)
+{
+	char *dup;
+
+	if (str == NULL)
+		return (NULL);
+	dup = malloc(sizeof(str) * strlen(str));
+	if (dup == NULL)
+		return (NULL);
+	dup = str;
+	return (dup);
+}

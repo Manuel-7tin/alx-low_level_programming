67f544843194c71f50ad1ecc40f1afa7ad1594ac
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-03-20 05:17:49 -0700

    Task0: CReating a simple struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
-typedef struct dog {
+/**
+ * struct dog - Struct of dog infos
+ *
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
+ * Description: Theres little to nothing left to say.
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;

00bbcbdb2e4172d71de4e8a4a4dd2bd1e840890e
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-03-20 15:04:36 -0700

    Task2: Prints out from struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,8 +17,8 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 			d->name = "(nil)";
-/*		if (d->age == NULL)
-			d->age = 0.00;*/
+		if (d->age == NULL)
+			d->age = 0.00;
 		if (d->owner == NULL)
 			d->owner = "(nil)";
 		printf("Name: %s\n", d->name);

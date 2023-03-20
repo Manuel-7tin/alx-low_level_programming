5b437050c3c717600dcf68363ed3c4a5da1c7224
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-03-20 15:00:23 -0700

    Task2: Prints out from struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stddef.h>
+#include <stdio.h>
 
 /**
  * print_dog - Prints from a dog struct
@@ -15,10 +17,10 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 			d->name = "(nil)";
-		if (d->age == NULL)
-			d->age = "(nil)";
+/*		if (d->age == NULL)
+			d->age = 0.00;*/
 		if (d->owner == NULL)
-			d->owner = "(nil)"
+			d->owner = "(nil)";
 		printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner);

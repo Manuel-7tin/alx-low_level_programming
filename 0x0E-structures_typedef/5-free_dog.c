bda33bb20d0224d0ea23d1c50a3c508a104bd9f6
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-03-20 21:30:12 -0700

    Task5: frees allocated space of struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees the memory occupied by a struct
+ *
+ * @d: The struct to free
+ *
+ * Description: Completely frees the memory where an inputted struct is stored
+ * Return: void
+ */
+
+void free(dog_t *d)
+{
+	free(d);
+}

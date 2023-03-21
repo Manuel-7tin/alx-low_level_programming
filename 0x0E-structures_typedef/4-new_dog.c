816151232d32a8184eb7f8822c3687dd85df98fc
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-03-21 05:18:55 -0700

    Task4: Create a new struct about dogs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,9 +22,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
+	{
+		free(new_dog);
 		return (NULL);
+	}
+	new_dog->name = malloc(1 + sizeof(name));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog->name);
+		return (NULL);
+	}
 	new_dog->name = name;
 	new_dog->age = age;
+	new_dog->owner = malloc(1 + sizeof(owner));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->owner);
+		return (NULL);
+	}
 	new_dog->owner = owner;
 	return (new_dog);
 }

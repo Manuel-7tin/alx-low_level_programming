c21cc205c50f64a1e1c792977dd9bb20addb25eb
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-04-04 16:02:13 -0700

    Task9: A function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned long idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	new_node->next = temp->next;
-	temp->next = new_node;
+	new_node->next = hold_node->next;
+	hold_node->next = new_node;
 	return (new_node);
 }

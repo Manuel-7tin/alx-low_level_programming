f8a83b4860b30b57bd06620a836d74fab73dc000
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-04-04 13:11:03 -0700

    Task7: A function that returns the nth node of a listint_t linked list.

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Gets the node of a list at indexn
+ *
+ * @head: The list to be transversed
+ * @index: the index number
+ *
+ * Return: returnsthe node at the index if found, returns null if not found
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *hold_node = head;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; i < index; i++)
+		hold_node = hold_node->next;
+	if (hold_node == NULL)
+		return (NULL);
+	return (hold_node);
+}

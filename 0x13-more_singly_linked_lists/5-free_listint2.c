36f9dfb86c1b1580cc9e15859303f432dab00d7b
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-04-04 06:44:58 -0700

    Task5: A function that frees a listint_t list.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees memory occcupied by a list
+ *
+ * @head: The list to be freed
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *hold_node;
+
+	while (*head != NULL)
+	{
+		hold_node = (*head)->next;
+		free(*head);
+		(*head) = hold_node;
+	}
+}

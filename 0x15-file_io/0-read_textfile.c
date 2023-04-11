d418ed37c4e3dd6a2bbeb9197606016db60ad10f
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-04-10 21:58:26 -0700

    Task0: A function that reads a text file and prints it to the POSIX standard output.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main"
+
+/**
+ * read_textfile - reads a text file and outpots iin
+ *
+ * @filename: The file to reaf thrcugh
+ * @letters: The number of letters to print
+ *
+ * Return: the number of chaar printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t d, m, w;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	d = open(filename, O_RDONLY | O_CREAT);
+	m = read(filename, buf, letters);
+	w = write(1, buf, letters);
+	if (d == -1 || m == -1 || w == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	close(d)
+	return (w);
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,7 @@
+10/April/2023
+
+ALX Software Engineering Program
+
+0x15.C - File i/o
+
+This folder consists of tasks on the topic of File i/o  in C  programming and linux.
